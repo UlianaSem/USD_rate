8463bb6d3fe37ed908a0e8b09f6ae6dde8ed7fb3
Uliana Semyonova <i@ulianasemyonova.ru>
2024-01-26 23:04:11 +0500

    Crete CurrentUSDRate view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from main.models import USDRate
+from main.serializers import USDRateSerializer
+from main.throttlings import CustomRateThrottle
+from main.utils import get_exchange_rate
+
+
+class CurrentUSDRate(APIView):
+    throttle_classes = [CustomRateThrottle]
+
+    def get(self, request, format=None):
+        current_rate = get_exchange_rate()
+        rates = USDRate.objects.all()[:10]
+
+        if current_rate is None:
+            return Response(
+                data={"current_rate": "Ошибка запроса к Apilayer",
+                      "last_rates": USDRateSerializer(rates, many=True).data},
+                status=status.HTTP_200_OK)
+
+        return Response(data={"current_rate": USDRateSerializer(current_rate).data,
+                              "last_rates": USDRateSerializer(rates, many=True).data},
+                        status=status.HTTP_200_OK)
