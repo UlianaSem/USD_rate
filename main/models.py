ed4738f72010e1ecbd131d130aaa80b61b6ab977
Uliana Semyonova <i@ulianasemyonova.ru>
2024-01-26 20:15:31 +0500

    Create USDRate model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class USDRate(models.Model):
+    rate = models.DecimalField(max_digits=10, decimal_places=6, verbose_name="значение курса")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+
+    def __str__(self):
+        return self.rate
+
+    class Meta:
+        verbose_name = "курс доллара"
+        verbose_name_plural = "курсы доллара"
+        ordering = ('-created_at', )
